50e3bd7d142e40bc07ce25e7ec0408c94f6f5219
Eduardo Jared Trujillo Gamero <167134639+JaredTrujillo01@users.noreply.github.com>
2025-11-24 22:49:29 -0600

    Terminado

--- DiLifetimesDemo/Program.cs
+++ DiLifetimesDemo/Program.cs
@@ -2,19 +2,19 @@ using DiLifetimesDemo.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Registro corregido: registra los tipos concretos para permitir inyección directa
+
 builder.Services.AddTransient<TransientServices>();
 builder.Services.AddScoped<ScopedServices>();
 builder.Services.AddSingleton<SingletonServices>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
